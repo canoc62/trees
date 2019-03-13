dda4f3039ebfcd56af0ee2b62699bed08ec28690
Christopher Cano <canoc4262@gmail.com>
2019-03-13 09:12:48 -0700

    Add clocking and add assignment large txt/test file.

--- medium_maintenance/main.py
+++ medium_maintenance/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from trees.bst import Tree
 
 def main():
@@ -7,11 +8,13 @@ def main():
     nums = str.split(data, "\n")
     nums = nums[0:len(nums)-1]
 
-    k_sum = k_median_sum(nums) 
-    print("k median sum is: " + str(k_sum))
+    k_sum, runtime = k_median_sum(nums) 
+    print("k median sum is: " + str(k_sum) + " and it took: " + str(runtime) + " s")
     f.close()
 
 def k_median_sum(nums):
+    start_time = time.clock()
+    
     root = Tree(int(nums[0]))
     sum = root.val 
 
@@ -24,6 +27,9 @@ def k_median_sum(nums):
         mid_val = root.select(mid) 
         sum += mid_val
     
-    return sum
+    end_time = time.clock()
+    length_time = end_time - start_time
+    
+    return sum, length_time
 
 main()
